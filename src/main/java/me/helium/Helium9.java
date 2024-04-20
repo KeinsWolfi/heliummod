e9766ece1dd19b59a41e809c94ed90a342f7fcbc
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2024-04-20 02:11:20 +0200

    Added a keybind for CreeperQuit as it was really annoying when fighting creepers willingly

--- src/main/java/me/helium/Helium9.java
+++ src/main/java/me/helium/Helium9.java
@@ -1,10 +1,18 @@
 package me.helium;
 
+import me.helium.mods.CreeperQuit;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.awt.event.KeyEvent;
 
 public class Helium9 implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -14,16 +22,32 @@ public class Helium9 implements ModInitializer {
 	public static final MinecraftClient mc = MinecraftClient.getInstance();
 
 	public static final String MOD_ID = "helium9";
-	public static final String MOD_NAME = "HeliumMod";
-	public static final String MOD_VERSION = "0.1.0";
+	public static ModMetadata MOD_META;
+	public static String MOD_NAME;
+	public static String MOD_VERSION;
 
+    private static KeyBinding creeperQuitKey;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		MOD_META = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().getMetadata();
+		MOD_NAME = MOD_META.getName();
+		MOD_VERSION = String.valueOf(MOD_META.getVersion());
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("MoinMoin meine aktiven freunde (" + MOD_NAME + " v" + MOD_VERSION + ")");
+
+		creeperQuitKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"CreeperQuit", // The translation key of the keybinding's name
+				KeyEvent.VK_G, // The keycode of the key
+				"Helium" // The translation key of the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (creeperQuitKey.wasPressed()) {
+				CreeperQuit.creeperQuit = !CreeperQuit.creeperQuit;
+				LOGGER.info("CreeperQuit toggled. (" + !CreeperQuit.creeperQuit + " to " + CreeperQuit.creeperQuit + ")");
+			}
+		});
+
 	}
 }
